54f082a27ba9dd400cf3009d6791af39ed6f1674
Bert Temme <btemme@hotmail.com>
2024-04-17 14:49:55 +0200

    add sources

---
+++ sibbe/README.md
@@ -0,0 +1,27 @@
+This is a minimal standalone version of the Tile3DLayer example
+on [deck.gl](http://deck.gl) website.
+
+### Usage
+
+Copy the content of this folder to your project. 
+
+To download the Cesium 3D tileset, you need a Cesium ion access token, and set it to `ION_TOKEN` in `app.jsx`.
+
+```bash
+# install dependencies
+npm install
+# or
+yarn
+# bundle and serve the app with vite
+npm start
+```
+
+### Data format
+
+Sample data is served by [Cesium ion](https://cesium.com/cesium-ion/).
+
+To use your own data, check out the [documentation of Tile3DLayer](../../../docs/api-reference/geo-layers/tile-3d-layer.md)
+
+### Basemap
+
+The basemap in this example is provided by [CARTO free basemap service](https://carto.com/basemaps). To use an alternative base map solution, visit [this guide](https://deck.gl/docs/get-started/using-with-map#using-other-basemap-services)
---
+++ sibbe/app.tsx
@@ -0,0 +1,66 @@
+import React, {useState} from 'react';
+import {createRoot} from 'react-dom/client';
+import {Map} from 'react-map-gl/maplibre';
+import DeckGL from '@deck.gl/react';
+import {Tile3DLayer} from '@deck.gl/geo-layers';
+import {TerrainLayer, TerrainLayerProps} from '@deck.gl/geo-layers';
+
+import type {MapViewState} from '@deck.gl/core';
+
+// const maptilerkey = 'NEeAOMvTyhTAwYbN30uo';
+const MAPBOX_TOKEN  = `pk.eyJ1IjoiYmVydHQiLCJhIjoiY2xsN3JrZ2p2MTR5NTNkbWtobDlqYXU3MyJ9.Rz_sNO5nv017M7K2nSBVFA`;
+const TILESET_URL = `https://geodan.github.io/pg2b3dm/sample_data/3dbag/sibbe/1.0/tileset.json`;
+const INITIAL_VIEW_STATE: MapViewState = {
+  latitude: 50.843332,
+  longitude: 5.825286,
+  pitch: 45,
+  maxPitch: 60,
+  bearing: 0,
+  minZoom: 2,
+  maxZoom: 30,
+  zoom: 17
+};
+
+const TERRAIN_IMAGE = `https://api.mapbox.com/v4/mapbox.terrain-rgb/{z}/{x}/{y}.png?access_token=${MAPBOX_TOKEN}`;
+const SURFACE_IMAGE = `https://api.mapbox.com/v4/mapbox.satellite/{z}/{x}/{y}@2x.png?access_token=${MAPBOX_TOKEN}`;
+
+const ELEVATION_DECODER: TerrainLayerProps['elevationDecoder'] = {
+  rScaler: 6553.6,
+  gScaler: 25.6,
+  bScaler: 0.1,
+  offset: -10000 + 44
+};
+
+export default function App({
+  texture = SURFACE_IMAGE,
+  wireframe = false,
+  initialViewState = INITIAL_VIEW_STATE
+}: {
+  texture?: string;
+  wireframe?: boolean;
+  initialViewState?: MapViewState;
+}) {
+  const layer = new TerrainLayer({
+    id: 'terrain',
+    minZoom: 0,
+    maxZoom: 23,
+    strategy: 'no-overlap',
+    elevationDecoder: ELEVATION_DECODER,
+    elevationData: TERRAIN_IMAGE,
+    texture,
+    wireframe,
+    color: [255, 255, 255]
+  });
+
+  const tile3DLayer = new Tile3DLayer({
+    id: 'tile-3d-layer',
+    pointSize: 2,
+    data: TILESET_URL
+  });
+
+  return <DeckGL initialViewState={initialViewState} controller={true} layers={[tile3DLayer,layer]} />;
+}
+
+export function renderToDOM(container: HTMLDivElement) {
+  createRoot(container).render(<App />);
+}
